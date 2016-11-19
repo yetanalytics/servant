0199bd278864882a8f83e34103479e5316317a5a
Milt Reder <milt@yetanalytics.com>
2016-11-19 13:48:13 -0500

    update deps

--- project.clj
+++ project.clj
@@ -1,14 +1,13 @@
-(defproject servant "0.1.5"
+(defproject servant "0.1.6-SNAPSHOT"
   :source-paths ["src/cljs"]
   :description "A Clojurescript Library for interacting with webworkers sanely"
   :url "https://github.com/MarcoPolo/Servant"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.7.228"]
-                 [org.clojure/core.async "0.2.374"]
-                 [com.cemerick/clojurescript.test "0.0.4"] ]
-  :plugins [[lein-cljsbuild "1.1.2"]]
+                 [org.clojure/clojurescript "1.9.293"]
+                 [org.clojure/core.async "0.2.395"]]
+  :plugins [[lein-cljsbuild "1.1.4"]]
   :cljsbuild
               {:builds
                [{:id "servant"
